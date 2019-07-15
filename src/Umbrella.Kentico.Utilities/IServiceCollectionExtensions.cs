22b6a0a874494096268a9a3ded1fe464d93347ae
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-07-15 23:07:09 +0100

    Updated to Kentico 12 and fixed breaking changes.

--- src/Umbrella.Kentico.Utilities/IServiceCollectionExtensions.cs
+++ src/Umbrella.Kentico.Utilities/IServiceCollectionExtensions.cs
@@ -1,46 +1,46 @@
-﻿using CMS.ContactManagement;
+﻿using System;
+using CMS.Activities.Loggers;
+using CMS.ContactManagement;
 using CMS.ContactManagement.Internal;
 using CMS.Core;
-using Umbrella.Kentico.Utilities.ContactManagement.Abstractions;
 using Umbrella.Kentico.Utilities.ContactManagement;
-using Umbrella.Kentico.Utilities.Users.Abstractions;
-using Umbrella.Kentico.Utilities.Users;
+using Umbrella.Kentico.Utilities.ContactManagement.Abstractions;
 using Umbrella.Kentico.Utilities.ContactManagement.Options;
-using System;
-using Umbrella.Kentico.Utilities.Middleware.Options;
 using Umbrella.Kentico.Utilities.Middleware;
-using Kentico.Activities;
+using Umbrella.Kentico.Utilities.Middleware.Options;
+using Umbrella.Kentico.Utilities.Users;
+using Umbrella.Kentico.Utilities.Users.Abstractions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-	public static class IServiceCollectionExtensions
+    public static class IServiceCollectionExtensions
     {
         public static IServiceCollection AddUmbrellaKenticoUtilities(this IServiceCollection services)
-		{
-			// Contact Management
-			services.AddScoped(ctx => Service.Entry<IContactProcessingChecker>());
-			services.AddScoped(ctx => Service.Entry<IContactCreator>());
-			services.AddScoped(ctx => Service.Entry<IContactRelationAssigner>());
-			services.AddScoped(ctx => Service.Entry<IContactPersistentStorage>());
-			services.AddScoped(ctx => Service.Entry<IContactMergeService>());
-			services.AddSingleton<IMembershipActivitiesLogger, MembershipActivitiesLogger>();
+        {
+            // Contact Management
+            services.AddScoped(ctx => Service.Resolve<IContactProcessingChecker>());
+            services.AddScoped(ctx => Service.Resolve<IContactCreator>());
+            services.AddScoped(ctx => Service.Resolve<IContactRelationAssigner>());
+            services.AddScoped(ctx => Service.Resolve<IContactPersistentStorage>());
+            services.AddScoped(ctx => Service.Resolve<IContactMergeService>());
+            services.AddSingleton<IMembershipActivityLogger, MembershipActivityLogger>();
 
-			services.AddScoped<IKenticoContactManager, KenticoContactManager>();
-			services.AddSingleton<IKenticoUserNameNormalizer, KenticoUserNameNormalizer>();
+            services.AddScoped<IKenticoContactManager, KenticoContactManager>();
+            services.AddSingleton<IKenticoUserNameNormalizer, KenticoUserNameNormalizer>();
 
-			// Default Options - These can be replaced by calls to the Configure* methods below.
-			services.AddSingleton<KenticoContactManagerOptions>();
+            // Default Options - These can be replaced by calls to the Configure* methods below.
+            services.AddSingleton<KenticoContactManagerOptions>();
 
-			// Middleware
-			services.AddScoped<MergeMarketingContactMiddleware>();
+            // Middleware
+            services.AddScoped<MergeMarketingContactMiddleware>();
 
-			return services;
-		}
+            return services;
+        }
 
-		public static IServiceCollection ConfigureKenticoContactManagerOptions(this IServiceCollection services, Action<IServiceProvider, KenticoContactManagerOptions> optionsBuilder)
-			=> services.ConfigureUmbrellaOptions(optionsBuilder);
+        public static IServiceCollection ConfigureKenticoContactManagerOptions(this IServiceCollection services, Action<IServiceProvider, KenticoContactManagerOptions> optionsBuilder)
+            => services.ConfigureUmbrellaOptions(optionsBuilder);
 
-		public static IServiceCollection ConfigureMergeMarketingContactMiddlewareOptions(this IServiceCollection services, Action<IServiceProvider, MergeMarketingContactMiddlewareOptions> optionsBuilder)
-			=> services.ConfigureUmbrellaOptions(optionsBuilder);
+        public static IServiceCollection ConfigureMergeMarketingContactMiddlewareOptions(this IServiceCollection services, Action<IServiceProvider, MergeMarketingContactMiddlewareOptions> optionsBuilder)
+            => services.ConfigureUmbrellaOptions(optionsBuilder);
     }
 }
