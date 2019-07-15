e4cf3c123d4fc9b3b20f1e9a66144003067d9e2e
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-07-15 23:11:55 +0100

    Added .editorconfig and version update powershell files from the main Umbrella Code Libraries repo. Updated the version number to 3.0.0-preview-001

---
+++ Update-Version.ps1
@@ -0,0 +1,37 @@
+$configFiles = Get-ChildItem . *.csproj -rec
+$affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
+
+# Manually alter the build number before pushing to NuGet feeds
+$previousBuild = "2.8.1"
+$currentBuild =  "3.0.0-preview-001"
+
+foreach ($file in $configFiles)
+{
+	$content = Get-Content $file.PSPath
+
+	if($content -like "*<Version>" + $previousBuild + "</Version>*")
+	{
+		$affectedFiles.Add($file.Name)
+		$content -replace ("<Version>" + $previousBuild + "</Version>"), ("<Version>" + $currentBuild + "</Version>") | Set-Content $file.PSPath
+	}
+
+	# Ensure Copyright year is correct
+	if($content -like "*<Copyright>Zinofi Digital Ltd 2018</Copyright>*")
+	{
+		$affectedFiles.Add($file.Name)
+		$content -replace ("<Copyright>Zinofi Digital Ltd 2018</Copyright>"), ("<Copyright>Zinofi Digital Ltd 2019</Copyright>") | Set-Content $file.PSPath
+	}
+}
+
+Write-Output ""
+Write-Output "Updating from $($previousBuild) to $($currentBuild)"
+Write-Output ""
+Write-Output "Total files: $($configFiles.Length)"
+Write-Output "Total affected files: $($affectedFiles.Count)"
+Write-Output ""
+
+if(!$affectedFiles.Count.Equals(0))
+{
+	Write-Output "Affected Files:"
+	$affectedFiles | ForEach-Object { Write-Output $_ }
+}
